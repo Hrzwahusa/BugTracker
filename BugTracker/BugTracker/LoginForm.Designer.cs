83a57a859c67e8f9d6ca2d1a82b80213b4b0f366
Hrzwahusa <76783197+Hrzwahusa@users.noreply.github.com>
2021-06-25 22:17:43 +0200

    Initial Project

---
+++ BugTracker/BugTracker/LoginForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace BugTracker
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.failedlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // username
+            // 
+            this.username.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.username.Location = new System.Drawing.Point(340, 128);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 22);
+            this.username.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(242, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // pass
+            // 
+            this.pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pass.Location = new System.Drawing.Point(340, 182);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(100, 22);
+            this.pass.TabIndex = 3;
+            this.pass.UseSystemPasswordChar = true;
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginbutton.Location = new System.Drawing.Point(340, 237);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(100, 34);
+            this.loginbutton.TabIndex = 4;
+            this.loginbutton.Text = "Login";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            this.loginbutton.Click += new System.EventHandler(this.loginbutton_Click);
+            // 
+            // failedlabel
+            // 
+            this.failedlabel.AutoSize = true;
+            this.failedlabel.Location = new System.Drawing.Point(337, 289);
+            this.failedlabel.Name = "failedlabel";
+            this.failedlabel.Size = new System.Drawing.Size(64, 13);
+            this.failedlabel.TabIndex = 5;
+            this.failedlabel.Text = "Login Failed";
+            this.failedlabel.Visible = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.failedlabel);
+            this.Controls.Add(this.loginbutton);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Button loginbutton;
+        private System.Windows.Forms.Label failedlabel;
+    }
+}
+
---
+++ BugTracker/BugTracker/LoginForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTracker
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }       
+
+        private void loginbutton_Click(object sender, EventArgs e)
+        {
+            //cleaning user input
+
+            String user = username.ToString();
+            String password = pass.ToString();
+
+            for(int i = 0; i < user.Length; i++)
+            {
+                if(user.Substring(i,1) == "\"" || user.Substring(i,1) == "'" || user.Substring(i,1) == "\\")
+                {
+                    failedlabel.Text = "Invalid Username";
+                    failedlabel.Visible = true;
+                }
+            }
+
+            for(int i = 0; i < password.Length; i++)
+            {
+                if(password.Substring(i, 1) == "\"" || password.Substring(i, 1) == "'" || password.Substring(i, 1) == "\\")
+                {
+                    failedlabel.Text = "Forbidden Characters in Password";
+                    failedlabel.Visible = true;
+                }
+            }
+
+            //execute login request
+
+            dblogin(user,password);
+            
+
+            
+        }
+        public void dblogin(String user, String pass)
+        {
+
+        }
+    }
+}
